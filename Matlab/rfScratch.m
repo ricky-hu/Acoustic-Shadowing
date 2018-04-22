38053d1e07f5cb29ef14d0e058b8b0f810396a5d
unknown <rickyhu.stu@ead.ubc.ca>
2018-04-21 19:32:04 -0700

    Added plots of distributions of RF data for different regions

--- Matlab/rfScratch.m
+++ Matlab/rfScratch.m
@@ -72,7 +72,71 @@ set(gcf,'color','white');
 % investigating the log(hilbert(rf)) figure as it provides clear indication
 % of shadow regions
 
-manualShadow = outlineShadow(bmode);
+boundaryThickness = 10;
+[manualShadowMatrix, boundaryShadowMatrix, deepShadowMatrix] = outlineShadow(bmode, fileName, boundaryThickness);
 
+% looking at the distributions of the rf data of each region (shadow, shadow boundary, and
+% deep shadow)
+
+[rows, cols] = size(rf);
+shadowRFVals = zeros(1, (rows*cols));
+boundaryRFVals = zeros(1, (rows*cols));
+deepShadowRFVals = zeros(1, (rows*cols));
+
+% have to do some indexing to keep track since data is large so the arrays
+% for each regionar epreallocated to teh max possible size, otherwise the
+% for loops take too long
+
+numShadowRFVals = 0;
+numBoundaryRFVals = 0;
+numDeepShadowRFVals = 0;
+
+% creating vectors for rf values for each region
+% this loop checks which region the current rf index matches and appends
+% the rf data to a vector associated with that region
+for colIdx = 1:cols
+    for rowIdx = 1:rows
+        if manualShadowMatrix(rowIdx, colIdx) == 0
+            shadowRFVals(1, numShadowRFVals + 1) = rf(rowIdx, colIdx);
+            numShadowRFVals = numShadowRFVals + 1;
+        end
+        if boundaryShadowMatrix(rowIdx, colIdx) == 0
+            boundaryRFVals(1, numBoundaryRFVals + 1) = rf(rowIdx, colIdx);
+            numBoundaryRFVals = numBoundaryRFVals + 1;
+        end
+        if deepShadowMatrix(rowIdx, colIdx) == 0
+            deepShadowRFVals(1, numDeepShadowRFVals + 1) = rf(rowIdx, colIdx);
+            numDeepShadowRFVals = numDeepShadowRFVals + 1;
+        end                 
+    end
+end
+
+% resizing arrays so the preallocated zeros are gone
+shadowRFVals = shadowRFVals(1,1:(numShadowRFVals));
+boundaryRFVals = boundaryRFVals(1,1:(numBoundaryRFVals));
+deepShadowRFVals = deepShadowRFVals(1,1:(numDeepShadowRFVals));
+
+% plotting histograms of rf values in each region
+% the minimum rf value is -32768 and the max is 32768
+
+figure(2)
+
+subplot(1,3,1);
+histogram(shadowRFVals, 'BinWidth', 1);
+xlabel('RF Value');
+ylabel('Number of elements');
+title('RF Distribution of Shadow Regions');
+
+subplot(1,3,2);
+histogram(boundaryRFVals, 'BinWidth', 1);
+xlabel('RF Value');
+ylabel('Number of elements');
+title('RF Distribution of Boundary Regions');
+
+subplot(1,3,3);
+histogram(deepShadowRFVals, 'BinWidth', 1);
+xlabel('RF Value');
+ylabel('Number of elements');
+title('RF Distribution of Deep Shadow Regions');
 
 
