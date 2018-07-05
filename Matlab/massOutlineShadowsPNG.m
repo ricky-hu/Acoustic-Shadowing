631748ea084a0020fc0631e0e85f731f1ce51819
unknown <rickyhu.stu@ead.ubc.ca>
2018-07-05 14:30:42 -0700

    fixed mismatched for loops in cycling through images

--- Matlab/massOutlineShadowsPNG.m
+++ Matlab/massOutlineShadowsPNG.m
@@ -3,7 +3,7 @@
 % input:    dirName:    the directory where .png bmode images are located
 % output:   saved .png files of the manual shadows
 
-function massOutlineShadows(dirName)
+function massOutlineShadowsPNG(dirName)
 files = dir(fullfile(dirName, '*_cropped.png'));
 [numFiles dummy] = size(files);
 
@@ -14,16 +14,17 @@ for n = 1:numFiles
      if exist([name '_manual.png'], 'file')
         %do nothing
      else
-            im = imread(files(n).name);
-            boundaryThickness = 10;
-            
-            % prompting user to manually outline shadoww
-            [manualShadowMatrix, boundaryShadowMatrix, deepShadowMatrix] = outlineShadow(im, fileName, boundaryThickness);
-            
-            % writing image, note that ROIpoly makes bounded region zero
-            % (white), whereas we wnat bounded region to be black
-            imwrite(~manualShadowMatrix, [name '_manual.png']);
-        end
+         im = imread(files(n).name);
+         fprintf('%s \n', name)
+         boundaryThickness = 10;
+         
+         
+         % prompting user to manually outline shadoww
+         [manualShadowMatrix, boundaryShadowMatrix, deepShadowMatrix] = outlineShadow(im, fileName, boundaryThickness);
+         
+         % writing image, note that ROIpoly makes bounded region zero
+         % (white), whereas we wnat bounded region to be black
+         imwrite(~manualShadowMatrix, [name '_manual.png']);
      end
 end
         
